4df4cd4a658c53f3642e28a87e2bc990ce1b4a29
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2024-04-19 18:23:27 +0700

    denny

--- app/View/Components/testing.php
+++ app/View/Components/testing.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class testing extends Component
+class Testing extends Component
 {
     /**
      * Create a new component instance.
