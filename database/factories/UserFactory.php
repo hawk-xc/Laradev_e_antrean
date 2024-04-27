daaf03ac5b3b0f9e6bcad77215241b5b4be74f9d
wahyutc <hawkxc068@gmail.com>
2024-04-27 11:49:52 +0700

    user factory set role_id

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
-            'role_id' => 3,
+            'role_id' => 4,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
