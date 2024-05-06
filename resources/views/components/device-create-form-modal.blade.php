28b20ca35a21f3481ac2f56e0e4ce888f4793c80
wahyutc <hawkxc068@gmail.com>
2024-05-06 10:25:14 +0700

    add update on device

---
+++ resources/views/components/device-create-form-modal.blade.php
@@ -0,0 +1,46 @@
+<div>
+    <dialog id="my_modal_4" class="modal" wire:ignore.self>
+        <div class="modal-box">
+            <h3 class="text-lg font-bold">Hello!</h3>
+            <div class="flex flex-col gap-2">
+                <form wire:submit='testing'>
+                    <label class="w-full max-w-xs form-control">
+                        <div class="label">
+                            <span class="label-text">Device name?</span>
+                        </div>
+                        <input type="text" placeholder="Type here" class="w-full max-w-xs input input-bordered"
+                            wire:model="device_name" />
+                        @error('device_name')
+                            <div class="label">
+                                <span class="label-text-alt">{{ $message }}</span>
+                            </div>
+                        @enderror
+                    </label>
+                    <label class="w-full max-w-xs form-control">
+                        <div class="label">
+                            <span class="label-text">Device year?</span>
+                        </div>
+                        <input type="number" min="1980" max="{{ now()->year }}" placeholder="Type here"
+                            class="w-full max-w-xs input input-bordered" wire:model="device_year" />
+                        @error('device_year')
+                            <div class="label">
+                                <span class="label-text-alt">{{ $message }}</span>
+                            </div>
+                        @enderror
+                    </label>
+                </form>
+            </div>
+            <div class="modal-action">
+
+                <div class="modal-action">
+                    <button wire:click="create" class="btn btn-neutral">save!</button>
+                    <form method="dialog" class="flex gap-3">
+                        <!-- if there is a button, it will close the modal -->
+                        <button id="closeButton" class="btn" wire:click='close'
+                            wire:loading.attr="disabled">Close</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </dialog>
+</div>
---
+++ resources/views/components/ticket-create-form-modal.blade.php
@@ -0,0 +1,46 @@
+<dialog id="my_modal_4" class="modal" wire:ignore.self>
+    <div class="w-11/12 max-w-5xl modal-box">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <span>create new Ticket</span>
+        <div class="flex flex-col gap-2">
+            <label class="max-w-xs w-96 form-control">
+                <div class="label">
+                    <span class="label-text">Select Device</span>
+                    @error('device_id')
+                        <span class="label-text-alt">{{ $message }}</span>
+                    @enderror
+                </div>
+                <select
+                    class="select select-bordered @error('device_id')
+                select-error
+                @enderror"
+                    wire:model="device_id">
+                    <option selected value="null">Pick one</option>
+                    @foreach ($devices as $device)
+                        <option value="{{ $device->id }}">{{ $device->device_name }}</option>
+                    @endforeach
+                </select>
+            </label>
+            <label class="form-control">
+                <div class="label">
+                    <span class="label-text">Description</span>
+                    @error('description')
+                        <span class="label-text-alt">{{ $message }}</span>
+                    @enderror
+                </div>
+                <textarea wire:model="description"
+                    class="h-36 textarea textarea-bordered @error('description')
+textarea-warning
+@enderror" rows="3"
+                    placeholder="Description..."></textarea>
+            </label>
+        </div>
+        <div class="modal-action">
+            <button class="btn btn-neutral" wire:click="create" wire:loading.attr="disabled">create</button>
+            <form method="dialog" class="flex gap-3">
+                <!-- if there is a button, it will close the modal -->
+                <button id="closeButton" class="btn" wire:click="fresh" wire:loading.attr="disabled">Close</button>
+            </form>
+        </div>
+    </div>
+</dialog>
