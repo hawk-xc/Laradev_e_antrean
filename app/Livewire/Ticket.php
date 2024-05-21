6a9876d82cf1332ce91da5708438b63f34dec57e
hawk-xc68 <92193431+hawk-xc@users.noreply.github.com>
2024-05-21 10:21:24 +0700

    Update Ticket.php

--- app/Livewire/Ticket.php
+++ app/Livewire/Ticket.php
@@ -2,55 +2,68 @@
 
 namespace App\Livewire;
 
-use \App\Models\Device;
-use \App\Models\Proces;
+use \App\Models\Device as DeviceModel;
 use \App\Models\Ticket as TicketModel;
+use \App\Models\Proces as ProcesModel;
 use DateTime;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\View\View;
 use Livewire\Component;
-use Livewire\WithoutUrlPagination;
-use Livewire\WithPagination;
-
+use Livewire\Attributes\Validate;
 
 class Ticket extends Component
 {
-    use WithPagination, WithoutUrlPagination;
+    public $id, $device_id, $description, $ticket_updated_at, $ticket_id, $closed_id;
 
-    public $ticket_id, $device_id, $description;
-    // public $count = 0;
-    public $openModal = true;
-    public $action = 'create';
-
-    public $notif = false;
-
-    // delete functionality
-    public $delete_id;
+    public $loadCount = 5;
     protected $listeners = ['confirmDelete' => 'deleteTicket'];
 
-    public function fresh(): void
+    public function fresh()
     {
-        $this->reset('device_id');
-        $this->reset('device_id');
-        $this->reset('description');
-        $this->action = 'create';
-        // $this->openModal = false;
+        $this->id = '';
+        $this->device_id = '';
+        $this->description = '';
+        $this->ticket_id = '';
+        $this->ticket_updated_at = '';
     }
 
-    public function create(): void
+    // load more load less config
+    public function loadMore(int $int): void
     {
-        $this->openModal = true;
-        $this->action = 'create';
+        $this->loadCount += $int;
+    }
+
+    public function loadLess(int $int): void
+    {
+        $this->loadCount -= $int;
+    }
+
+    public function loadAll(): void
+    {
+        $this->loadCount = \App\Models\Ticket::where('device_id', \App\Models\Device::where('user_id', Auth::user()->id)->pluck('id'))->count();
+    }
+
+    public function loadAllLess(): void
+    {
+        $this->loadCount = 5;
+    }
+
+    public function insert_testing()
+    {
+        // $this->fresh();
+        $this->resetValidation(['device_id', 'description']);
+    }
+
+    public function store_testing()
+    {
+        // dd($this);
         $validate = $this->validate([
-            'device_id' => 'required',
-            'description' => 'required|min:3'
+            'device_id' => 'required|integer',
+            'description' => 'required|min:3',
         ]);
 
         $validate['created_at'] = now()->format('Y-m-d H:i:s');
-        // $validate['created_at'] = now()->addDay(1)->format('Y-m-d H:i:s');
         $validate['closed_at'] = now()->addDay(3)->format('Y-m-d H:i:s');
 
-        // Membandingkan tanggal saja, tanpa waktu
         $today = now()->format('Y-m-d');
         $count = TicketModel::whereDate('created_at', $today)->count();
         if ($validate['created_at'] == now()) {
@@ -59,86 +72,91 @@ class Ticket extends Component
             $validate['id_ticket'] = now()->format('ymd') . ($count = 1);
         }
 
-        if ($validate) {
+        if ($validate) :
             $ticket = TicketModel::create($validate);
             $data = [
                 'status_id' => 1,
                 'ticket_id' => $ticket->id,
             ];
 
-            $create = Proces::create($data);
+            $create = ProcesModel::create($data);
             if ($create) {
                 $this->dispatch('notify', type: 'success', message: 'data successfully created!');
                 event(new \App\Events\UserInteraction(Auth::user(), "Ticket => create ticket " . $create->id));
                 $this->fresh();
             }
             $this->dispatch('closeButton');
+        endif;
+    }
+
+    public function edit_testing(int $id): void
+    {
+        $ticket = \App\Models\Ticket::find($id);
+        $this->id = $id;
+        $this->device_id = $ticket->device->id;
+        $this->description = $ticket->description;
+        $this->ticket_updated_at = $ticket->updated_at;
+        $this->ticket_id = $id;
+
+        $this->resetValidation(['device_name', 'device_year']);
+
+        $this->dispatch('modals-ticket', data: $ticket);
+
+        // dd($this);
+    }
+
+    public function update_testing(): void
+    {
+        $validate = $this->validate([
+            'device_id' => 'required|numeric',
+            'description' => 'required|min:3',
+        ]);
+
+        $ticket = TicketModel::find($this->ticket_id);
+
+        event(new \App\Events\UserInteraction(Auth::user(), "Ticket => update ticket " . $ticket->id));
+
+        if ($create = $ticket->update($validate)) {
+            $this->dispatch('notify', type: 'success', message: 'data successfull updated!');
+            $this->fresh();
+            $this->dispatch('closeButton');
         }
     }
 
+    public function deleteConfirmation(): void
+    {
+        $this->dispatch('closeButton');
+        $this->dispatch('show-delete');
+        $this->closed_id = $this->ticket_id;
+    }
+
+    public function deleteTicket(): void
+    {
+        $update = ProcesModel::where('ticket_id', $this->closed_id)->update(['status_id' => 5]);
+        // $ticket = TicketModel::find($this->delete_id);
+
+        if (ProcesModel::where('ticket_id', $this->closed_id)->first()->status_id === 5) {
+            event(new \App\Events\UserInteraction(Auth::user(), "Ticket => closed ticket " . $this->closed_id));
+            if ($update) {
+                $this->dispatch('notify', type: 'success', message: 'data berhasil diupdate!');
+                $this->fresh();
+            }
+        }
+    }
 
     public function close()
     {
         $this->fresh();
     }
 
-    public function edit(int $id): void
+    public function render()
     {
-        $this->action = 'edit';
-        $this->openModal = true;
-        $ticket = TicketModel::find($id);
-        $this->ticket_id = $id;
-        $this->device_id = $ticket->device_id;
-        $this->description = $ticket->description;
-    }
-
-    public function store()
-    {
-        $validate = $this->validate([
-            'device_id' => 'required|numeric',
-            'description' => 'required|min:3'
-        ]);
-
-        $ticket = TicketModel::find($this->ticket_id);
-        event(new \App\Events\UserInteraction(Auth::user(), "Ticket => update ticket " . $ticket->id));
-
-        if ($create = $ticket->update($validate)) {
-            $this->dispatch('notify', type: 'success', message: 'data successfull updated!');
-
-            $this->fresh();
-            $this->dispatch('closeButton');
-        }
-    }
-
-    public function deleteConfirmation(int $id): void
-    {
-        $this->delete_id = $id;
-        $this->dispatch('show-delete');
-        $this->dispatch('closeButton');
-    }
-
-    public function deleteTicket(): void
-    {
-        Proces::where('ticket_id', $this->delete_id)->delete();
-        $ticket = TicketModel::find($this->delete_id);
-
-        event(new \App\Events\UserInteraction(Auth::user(), "Ticket => delete ticket " . $ticket->id));
-        if ($create = $ticket->delete()) {
-            $this->dispatch('notify', type: 'success', message: 'data successfully deleted!');
-            $this->fresh();
-        }
-    }
-
-    public function render(): View
-    {
-        $devices = Device::where('user_id', Auth::user()->id)->get();
-        $tickets = TicketModel::whereIn('device_id', $devices->pluck('id'))->orderBy('created_at', 'asc')->paginate(5);
-
-        // session()->flash('notify', 'data successfull delete!');
-        return view('livewire.ticket', [
+        $auth = Auth::user();
+        $devices = DeviceModel::where('user_id', Auth::user()->id)->get();
+        $tickets = TicketModel::whereIn('device_id', $devices->pluck('id'))->orderBy('created_at', 'asc')->limit($this->loadCount)->get();
+        return view('livewire.ticket')->with([
             'tickets' => $tickets,
             'devices' => $devices,
-            // 'description' => $this->description
         ]);
     }
 }
