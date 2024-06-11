9cbfa5c2326c693c27c3409ef78d6e24f7dd614c
wahyutc <hawkxc068@gmail.com>
2024-06-11 12:57:50 +0700

    update landing page, apis migration

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -7,37 +7,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <nav class="w-full flex justify-between px-10 py-3 backdrop-blur-lg brightness-75 shadow-xl max-sm:text-xs">
+    <nav class="flex justify-between w-full px-10 py-3 shadow-xl backdrop-blur-lg brightness-75 max-sm:text-xs">
         <span>e-service</span>
         <span>
             <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="m-1">panel</div>
-            <ul tabindex="0" class="dropdown-content translate-x-8 z-[1] menu p-2 shadow backdrop-blur-md mt-4 rounded-box w-52">
-              <li><a href="{{ route('login') }}">login</a></li>
-              <li><a href="{{ route('register') }}">register</a></li>
-            </ul>
-          </div>
+                <div tabindex="0" role="button" class="m-1">panel</div>
+                <ul tabindex="0"
+                    class="dropdown-content translate-x-8 z-[1] menu p-2 shadow backdrop-blur-md mt-4 rounded-box w-52">
+                    <li><a href="{{ route('login') }}">login</a></li>
+                    <li><a href="{{ route('register') }}">register</a></li>
+                </ul>
+            </div>
         </span>
     </nav>
-    <title>halaman e-service</title>
+    <title>halaman ABC-Komputer</title>
 </head>
-<body class="h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url('{{ asset('images/background.png') }}')">
-    <div class="w-full h-screen flex justify-center align-middle items-center flex-col"  data-aos="zoom-in-down">
-        <span class="text-9xl max-sm:text-[4rem] font-extrabold font-times bg-gradient-to-r from-primary to-blue-800 opacity-90 bg-clip-text text-transparent">e-service</span>
+
+<body class="w-screen h-screen bg-center bg-no-repeat bg-cover"
+    style="background-image: url('{{ asset('images/background.png') }}')">
+    <div class="flex flex-col items-center justify-center w-full h-screen align-middle" data-aos="zoom-in-down">
+        <span
+            class="text-8xl max-sm:text-[4rem] font-extrabold font-times bg-gradient-to-r from-primary to-blue-800 opacity-90 bg-clip-text text-transparent">ABC-Komputer</span>
         <span class="flex flex-row gap-4 p-5">
             <div class="badge badge-neutral">cepat</div>
             <div class="badge badge-primary">efisien</div>
             <div class="badge badge-secondary">integritas</div>
         </span>
         <span class="w-[40rem] text-center max-sm:text-xs max-sm:w-72">
-            aplikasi e-service merupakan sebuah layanan multiplatform yang dirancang untuk mempermudah proses pengelolaan data service pernangkat komputer anda, aplikasi ini dapat mempercepat dan mempermudah proses pendaftaran antrean melalui aplikasi membuat perbaikan menjadi efisien.
+            aplikasi e-service merupakan sebuah layanan multiplatform yang dirancang untuk mempermudah proses
+            pengelolaan data service pernangkat komputer anda, aplikasi ini dapat mempercepat dan mempermudah proses
+            pendaftaran antrean melalui aplikasi membuat perbaikan menjadi efisien.
         </span>
-        <span class="mt-32 font-light text-xs text-slate-200">&copy; 2023-2024 tim e-service</span>
+        <span class="mt-32 text-xs font-light text-slate-200">&copy; 2023-2024 tim ABC-Komputer</span>
     </div>
 </body>
 
 <script>
     AOS.init();
-  </script>
-  
+</script>
+
 </html>
