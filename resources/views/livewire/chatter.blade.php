378e5a449b61257deda9e5f23024430ec45a6347
wahyutc <hawkxc068@gmail.com>
2024-06-12 22:14:43 +0700

    return chatter version

--- resources/views/livewire/chatter.blade.php
+++ resources/views/livewire/chatter.blade.php
@@ -3,11 +3,8 @@
         @forelse ($notifications as $notification)
             <div id="messageCard"
                 class="flex flex-row items-center justify-between pl-1 pr-5 align-middle transition-all duration-150 border rounded-lg cursor-pointer border-slate-200 even:bg-slate-50 hover:bg-slate-200"
-                wire:click="selectMessage({{ $notification->id }})">
-                <div class="flex flex-row items-center gap-3 p-1 align-middle">
-                class="border border-slate-200 even:bg-slate-50 rounded-lg flex flex-row justify-between align-middle items-center pl-1 pr-5 cursor-pointer hover:bg-slate-200 duration-150 transition-all"
                 wire:click="selectMessage({{ $notification->id }})" wire:poll.1s>
-                div class="flex flex-row p-1 gap-3 align-middle items-center">
+                <div class="flex flex-row items-center gap-3 p-1 align-middle">
                     <div
                         class="flex items-center justify-center h-10 text-white align-middle rounded-full shadow-sm aspect-square bg-neutral">
                         {{ strtoupper(substr($notification->name, 0, 1)) }}
@@ -17,9 +14,8 @@
                         <span class="text-xs">{{ $notification->created_at->diffForHumans() }}</span>
                     </span>
                 </div>
-                <span><i class="text-green-500 ri-circle-fill"></i></span
-                @if(\App\Models\Notification::where('user_id', $notification->id)->where('is_read', 0)->count() > 0)
-                    <span><i class="ri-circle-fill text-green-500"></i></span>
+                @if (\App\Models\Notification::where('user_id', $notification->id)->where('is_read', 0)->count() > 0)
+                    <span><i class="text-green-500 ri-circle-fill"></i></span>
                 @endif
             </div>
         @empty
@@ -43,7 +39,8 @@
                 </div>
             @endforeach
         </div>
-        <div id="chatpadd" class="w-full flex flex-row gap-2 justify-center align-middle items-center {{ $openTextEditor ? null : 'hidden' }}">
+        <div id="chatpadd"
+            class="w-full flex flex-row gap-2 justify-center align-middle items-center {{ $openTextEditor ? null : 'hidden' }}">
             <textarea id="expandingTextarea"
                 class="w-full h-[43px] p-2 text-base border border-gray-300 rounded-md resize-none overflow-hidden"
                 placeholder="Type your text here..." wire:ignore.self wire:model.live="Message"></textarea>
